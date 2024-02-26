dd8c7a4df3998e34c206bc293828db4fb6b1b717
hyungjin.lee <hyungjin.lee@emotion.co.kr>
2024-02-26 10:44:23 +0900

    init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import "../styles/global.css";
+export default function Detail() {
+  return (
+      <div>
+        aaaaa
+          <Link href={'/dir'}>aaa</Link>
+      </div>
+  );
+}
